a3d919f01dafe6e5ad760ec766d76cf6d3678272
Brendan Milton <brendan.milton@valiant.finance>
2025-08-09 07:45:11 +1000

    feat: 05 error tracking

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
-import { useMutation, useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
- const users = useQuery(api.users.getMany);
  const addUser = useMutation(api.users.add);
  
  return (
@@ -16,9 +15,6 @@ export default function Page() {
         <UserButton />
         <OrganizationSwitcher hidePersonal={true} />
         <Button onClick={() => addUser()}>Add</Button>
-        <div className="max-w-sm w-full mx-auto">
-          <p>{JSON.stringify(users, null, 2)}</p>
-        </div>
       </div> 
   </>
   )
