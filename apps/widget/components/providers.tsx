0a7f8ed6d283dc8af98520ed11d62957b0cf399f
Brendan Milton <brendan.milton@valiant.finance>
2025-08-16 07:09:35 +0800

    feat: 11 add default widget views

--- apps/widget/components/providers.tsx
+++ apps/widget/components/providers.tsx
@@ -1,15 +1,17 @@
 "use client"
 
 import * as React from "react"
-import { ConvexProvider, ConvexReactClient } from "convex/react"
-import { api } from "@workspace/backend/_generated/api"
+import { Provider } from "jotai";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "")
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "");
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ConvexProvider client={convex}>
-      {children}
+      <Provider>
+        {children}
+      </Provider>
     </ConvexProvider>
-  )
-}
+  );
+};
