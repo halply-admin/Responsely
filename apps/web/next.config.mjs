33b1c3a3369c67c913cb8e9df6b40fd9adfe93d9
Brendan Milton <brendan.milton@valiant.finance>
2025-09-09 05:13:59 +1000

    update middleware and config with location of landing page to avoid deployment error

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,18 +1,23 @@
-import { withSentryConfig } from "@sentry/nextjs";
+import {withSentryConfig} from "@sentry/nextjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
   devIndicators: false,
-
-  // Force disable standalone output (Vercel adds it automatically otherwise)
-  output: undefined,
-};
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/landing",
+        permanent: false,
+      },
+    ]
+  }
+}
 
 export default withSentryConfig(nextConfig, {
   org: "responsely",
   project: "responsely",
-
   silent: !process.env.CI,
   widenClientFileUpload: true,
   tunnelRoute: "/monitoring",
