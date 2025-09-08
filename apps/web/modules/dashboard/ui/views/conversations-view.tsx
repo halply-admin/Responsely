bb4c02fbcd46f6b4788785d4e435fdcf592ab42e
Brendan Milton <brendan.milton@valiant.finance>
2025-09-09 07:53:41 +1000

    replace logo with image

--- apps/web/modules/dashboard/ui/views/conversations-view.tsx
+++ apps/web/modules/dashboard/ui/views/conversations-view.tsx
@@ -3,9 +3,15 @@ import Image from "next/image";
 export const ConversationsView = () => {
   return (
     <div className="flex h-full flex-1 flex-col gap-y-4 bg-muted">
-      <div className="flex flex-1 items-center justify-center gap-x-2">
-        <Image alt="Logo" height={40} width={40} src="/logo.svg" />
-        <p className="font-semibold text-lg">Responsely</p>
+      <div className="flex flex-1 items-center justify-center">
+        <Image
+          src="/support-image.png"
+          alt="Responsely Logo"
+          width={200}     // base width
+          height={50}     // base height
+          className="h-auto w-[150px] sm:w-[200px] lg:w-[240px] object-contain"
+          priority
+        />
       </div>
     </div>
   );
